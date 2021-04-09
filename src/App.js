e29e798a437d8ba60313f1060048f56c23f863ca
Dimi Lee <bashruv@Dimis-MacBook-Pro.local>
2021-04-09 17:27:50 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          유해사이트 접속 시도가 확인되어 차단되었습니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="comment">
+          서울영상고등학교<br />3D 모델링실 관리자
+        </p>
       </header>
     </div>
   );
